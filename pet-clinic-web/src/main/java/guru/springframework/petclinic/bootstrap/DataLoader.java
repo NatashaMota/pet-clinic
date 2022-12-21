64917a9272257bfa262254400a0d57bd2eb1b50a
MotaNT <natasha.silva4@fatec.sp.gov.br>
2022-12-21 00:16:52 -0300

    initial bootstrap dataloader created. closes #9

---
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,42 @@
+package guru.springframework.petclinic.bootstrap;
+
+import guru.springframework.petclinic.model.Owner;
+import guru.springframework.petclinic.model.Vet;
+import guru.springframework.petclinic.services.OwnerService;
+import guru.springframework.petclinic.services.VetService;
+import guru.springframework.petclinic.services.map.OwnerServiceMap;
+import guru.springframework.petclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Indiana");
+        owner1.setLastName("Jones");
+
+        ownerService.save(owner1);
+
+        Vet vet1 = new Vet();
+        vet1.setFirstName("Doutor");
+        vet1.setLastName("Gatos Silva");
+        vet1.setId(1L);
+
+        vetService.save(vet1);
+        System.out.println("data saved");
+
+
+    }
+}
